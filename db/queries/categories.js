8bbadf8d9f10bab54f063c39f67563233309bdf8
Raymond Lin <raymond.lin.7690@gmail.com>
2023-06-24 18:33:44 -0700

    Adding Select queries for users and categories table

---
+++ db/queries/categories.js
@@ -0,0 +1,14 @@
+const db = require('../connection');
+
+//Get categories
+const getCategories = () => {
+  return db.query('SELECT * FROM categories;')
+    .then(data => {
+      return data.rows;
+    })
+    .catch((err) => {
+      console.log(err.messsage)
+    })
+};
+
+module.exports = { getCategories };
