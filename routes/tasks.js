07f9f306ae677d7fab27a26e7b3b75d22344f26f
Raymond Lin <raymond.lin.7690@gmail.com>
2023-06-28 14:38:45 -0700

    implementing open ai api to help categorise user tasks based on their prompt

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { addTask } = require('../db/queries/tasks');
+const { Configuration, OpenAIApi } = require("openai");
 
 router.get('/', (req, res) => {
   res.render('tasks');
@@ -10,21 +11,58 @@ router.get('/add', (req,res) => {
   res.render('add-task');
 });
 
-router.post('/', (req, res) => {
+const config = new Configuration({
+  apiKey: "",
+});
+
+const openai = new OpenAIApi(config);
+//prompt for ai to categorise userinput (wip)
+const runPrompt = async (taskName) => {
+  const prompt = `If you can categorise ${taskName} based on one of the following categories: 1. Eat, 2. Watch, 3. Read, 4. Buy, 5. Others, which would it be?`;
+
+  const response = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: prompt,
+    max_tokens: 2048,
+    temperature: 1,
+  });
+
+  const category = response.data.choices[0].text.trim();
+  return category;
+};
+
+router.post('/', async (req, res) => {
   const taskData = req.body;
   taskData.user_id = 1;
   const date = new Date().toJSON();
   taskData.date_created = date;
   console.log(taskData);
+
   addTask(taskData)
-  .then((result) => {
-    console.log(result);
-  })
-  .catch(err => console.log(err));
+    .then((result) => {
+      console.log(result);
+      res.redirect('/tasks');
+    })
+    .catch(err => console.log(err));
 
-  //function to get all Tasks for user , .then ((result) => res.redirect('/', {result}), add .err after
+  const userSubmission = taskData.task_name;
+  const category = await runPrompt(userSubmission);
 
-})
+  const formattedResponse = {
+    id: taskData.task,
+    category_id: taskData.category,
+    user_id: 1,
+    task_name: taskData.task.name,
+    task_description: taskData.task_description,
+    date_created: new Date().toJSON(),
+    date_completed: null,
+    category: category,
+  };
 
+  console.log("Formatted Response:", formattedResponse);
+
+  // Perform further actions based on the categorized task
+  res.json(formattedResponse);
+});
 
 module.exports = router;
